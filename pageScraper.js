dda9c70d5b3460ccb5749ae46ae50fa8fbeddc1f
Davide Pacco <72199035+HolaSoyGonzalo@users.noreply.github.com>
2021-04-18 17:50:22 +0200

    Setting Up Puppeteer

---
+++ pageScraper.js
@@ -0,0 +1,10 @@
+const scraperObject = {
+  url: "https://blu-verd.com/product-category/women/",
+  async scraper(browser) {
+    let page = await browser.newPage();
+    console.log(`Navigating to ${this.url}...`);
+    await page.goto(this.url);
+  },
+};
+
+module.exports = scraperObject;
